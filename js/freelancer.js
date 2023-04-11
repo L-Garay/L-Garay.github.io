f27e8d9264de8a1c87ed96248982c863846397b8
L-Garay <garay.logan@gmail.com>
2023-04-11 14:47:03 -0600

    show link to top button on desktop and fix marquee on mobile

--- js/freelancer.js
+++ js/freelancer.js
@@ -18,7 +18,7 @@
   // Scroll to top button appear
   $(document).scroll(function() {
     var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
+    if (scrollDistance > 500) {
       $('.scroll-to-top').fadeIn();
     } else {
       $('.scroll-to-top').fadeOut();
