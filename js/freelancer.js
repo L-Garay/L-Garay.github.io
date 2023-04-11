8dff00c2bfece5fd055414e2d1c015520a6de08c
L-Garay <garay.logan@gmail.com>
2023-04-11 14:04:55 -0600

    add marquee banner to site

--- js/freelancer.js
+++ js/freelancer.js
@@ -2,7 +2,7 @@
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click',function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -26,7 +26,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').on('click',function() {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -40,14 +40,16 @@
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
+      $("#marquee").addClass("marquee-shrink");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
+      $("#marquee").removeClass("marquee-shrink");
     }
   };
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+  $(window).on('scroll', navbarCollapse);
 
   // Floating label headings for the contact form
   $(function() {
